59d24606a7a41fbba992672e77515f414280e393
Viktoriia <viktoriamyhailiak@gmail.com>
2026-01-15 21:49:48 +0200

    add a solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,58 @@
 /// <reference types="cypress" />
 
+const username = 'tomsmith';
+const pw = 'SuperSecretPassword!';
+
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should sign in with correct creds', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(pw);
+    cy.get('.radius').click();
 
+    cy.url().should('include', '/secure');
+
+    cy.get('.flash.success')
+      .should('be.visible')
+      .and('contain', 'You logged into a secure area!');
+  });
+
+  it(`shouldn't sign in with incorrect creds`, () => {
+    cy.get('#username').type('tom');
+    cy.get('#password').type('tom');
+    cy.get('.radius').click();
+
+    cy.url().should('include', '/login');
+
+    cy.get('.flash.error')
+      .should('be.visible')
+      .and('contain', 'Your username is invalid!');
+  });
+});
+
+describe('Sign Out page', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.get('#username').type(username);
+    cy.get('#password').type(pw);
+    cy.get('.radius').click();
+
+    cy.url().should('include', '/secure');
+
+    cy.get('.flash.success')
+      .should('be.visible')
+      .and('contain', 'You logged into a secure area!');
+  });
+
+  it('should sign out', () => {
+    cy.url().should('include', '/secure');
+    cy.get('.button.secondary.radius').click();
+
+    cy.get('.flash.success')
+      .should('be.visible')
+      .and('contain', 'You logged out of the secure area!');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder=${placeholder}]`);
+});
+
+Cypress.Commands.add('assertPageUrl', (url) => {
+  cy.url().should('equal', Cypress.config().baseUrl + '/#' + url);
+});
